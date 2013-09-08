056c6ccac04a548748f0dade378b4b81c4ea3182
Eric Teubert <eric@ericteubert.de>
2013-09-08 16:50:06 +0200

    simplify pwp module

--- lib/modules/podlove_web_player/podlove_web_player.php
+++ lib/modules/podlove_web_player/podlove_web_player.php
@@ -9,17 +9,11 @@ class Podlove_Web_Player extends \Podlove\Modules\Base {
 
 	public function load() {
 
-		add_action( 'podlove_dashboard_meta_boxes', array( $this, 'register_meta_boxes' ) );
 		add_filter( 'the_content', array( $this, 'autoinsert_into_content' ) );
 
-		if ( defined( 'PODLOVEWEBPLAYER_DIR' ) ) {
-			define( 'PODLOVE_MEDIA_PLAYER', 'external' );
-			return;
-		} else {
-			define( 'PODLOVE_MEDIA_PLAYER', 'internal' );
+		if ( !defined( 'PODLOVEWEBPLAYER_DIR' ) ) {
+			include_once 'player/podlove-web-player/podlove-web-player.php';
 		}
-
-		include_once 'player/podlove-web-player/podlove-web-player.php';
 	}
 
 	public function autoinsert_into_content( $content ) {
@@ -39,23 +33,6 @@ class Podlove_Web_Player extends \Podlove\Modules\Base {
 		}
 
 		return $content;
-	}
-
-	public function register_meta_boxes() {
-		add_meta_box(
-			\Podlove\Podcast_Post_Type::SETTINGS_PAGE_HANDLE . '_player',
-			__( 'Webplayer', 'podlove' ),
-			array( $this, 'about_player_meta_box' ),
-			\Podlove\Podcast_Post_Type::SETTINGS_PAGE_HANDLE,
-			'side'
-		);
-	}
-
-	public function about_player_meta_box() {
-		if ( PODLOVE_MEDIA_PLAYER === 'external' )
-			echo __( 'It looks like you have installed an <strong>external plugin</strong> using mediaelement.js.<br>That\'s what\'s used.', 'podlove' );
-		else
-			echo __( 'Podlove ships with its <strong>own webplayer</strong>.<br>That\'s what\'s used.', 'podlove' );
 	}
 
 	public static function there_is_a_player_in_the_content( $content ) {

3ef580294db89ea7124230a1f9790b3f28de4649
chemiker <alex@nb-168-135.hirnport.ruhr-uni-bochum.de>
2013-12-06 13:30:21 +0100

    [WIP] Display iFrame

--- lib/modules/oembed/oembed.php
+++ lib/modules/oembed/oembed.php
@@ -15,9 +15,27 @@ class oembed extends \Podlove\Modules\Base {
 	}
 
 	public function load_oembed() {
+		if ( isset( $_GET['service'] ) && strtoupper( $_GET['service'] ) == "PODLOVE-WEB-PLAYER" ) {
+			if( is_single() ) : ?>
+				<html>
+				<head>
+				<?php
+				print_r(  wp_head() );
+				?>
+				</head>
+				<body>
+				<?php
+					print_r( $this->get_webplayer( get_the_ID() ) );
+				?>
+				</body>
+				</html>
+			<?php endif;
+			exit();
+		}
+
 		if( isset( $_GET['service'] ) && strtoupper( $_GET['service'] ) == "PODLOVE-OEMBED" &&
-			isset( $_GET['format'] ) ) :
-			if( is_single() ) :
+			isset( $_GET['format'] ) ) {
+			if( is_single() ) {
 				switch ( strtoupper( $_GET['format'] ) ) {
 					case 'JSON' :
 						header('Content-type: application/json; charset=utf-8');
@@ -36,12 +54,18 @@ class oembed extends \Podlove\Modules\Base {
 					break;
 				}
 				exit();
-			endif;
-		endif;
+			}
+		}
+	}
+
+	public function get_webplayer( $post_id ) {
+		$episode = \Podlove\Model\Episode::find_or_create_by_post_id( get_the_ID() );
+		$printer = new \Podlove\Modules\PodloveWebPlayer\Printer( $episode );
+		return $printer->render();
 	}
 
 	public function get_current_episode( $post_id ) {
-		if( get_post_status( $post_id ) !== 'publish' ) { // If post is not public, 404 will be replied
+		if( get_post_status( $post_id ) !== 'publish' || get_post_type( $post_id ) !== 'podcast' ) { // If post is not public, 404 will be replied
 			status_header( 404 );
 			return;
 		}
@@ -51,23 +75,26 @@ class oembed extends \Podlove\Modules\Base {
 
 		return array(		'version'		=> '1.0',
 							'type'			=> 'rich',
-							'width'			=> '',
-							'height'		=> '',
+							'width'			=> '600px',
+							'height'		=> '200px',
 							'title'			=> $episode->full_title(),
 							'url'			=> get_permalink( $post_id ),
 							'author_name'	=> $podcast->full_title(),
 							'author_url'	=> site_url(),
-							'thumbnail_url'	=> $episode->get_cover_art_with_fallback());
+							'thumbnail_url'	=> $episode->get_cover_art_with_fallback(),
+							'html'			=> '<iframe src="' . get_permalink( $post_id ) .'?service=podlove-web-player"></iframe>');
 	}
 
 	public function register_oembed_discovery() { // WordPress does not allow registering custom <link> elements.
 		$post_id = get_the_ID();
-		echo "	<link rel='alternate' type='application/json+oembed'
- 					href='" . get_permalink( $post_id ) . "?service=podlove-oembed&format=json'
- 					title='" . get_the_title( $post_id ) . " oEmbed Profile' />\n
-				<link rel='alternate' type='text/xml+oembed'
-  					href='" . get_permalink( $post_id ) . "?service=podlove-oembed&format=xml'
-  					title='" . get_the_title( $post_id ) . " oEmbed Profile' />\n";
+		if( is_single() && get_post_type( $post_id ) == 'podcast' ) {
+			echo "	<link rel='alternate' type='application/json+oembed'
+ 						href='" . get_permalink( $post_id ) . "?service=podlove-oembed&format=json'
+ 						title='" . get_the_title( $post_id ) . " oEmbed Profile' />\n
+					<link rel='alternate' type='text/xml+oembed'
+  						href='" . get_permalink( $post_id ) . "?service=podlove-oembed&format=xml'
+  						title='" . get_the_title( $post_id ) . " oEmbed Profile' />\n";
+  		}
 	}
 
 }
